69434fa55e6129a57fb6d5d8c93fdd1d356a5330
louisomo <aisagbonhilouis@gmail.com>
2023-02-01 19:06:01 +0100

    first commit

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Feature from '../../components/feature/Feature';
+import './features.css';
+
+const featuresData = [
+  {
+    title: 'Improving end distrusts instantly',
+    text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    title: 'Become the tended active',
+    text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    title: 'Message or am nothing',
+    text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    title: 'Really boy law county',
+    text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+];
+
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+      <p>Request Early Access to Get Started</p>
+    </div>
+    <div className="gpt3__features-container">
+      {featuresData.map((item, index) => (
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+      ))}
+    </div>
+  </div>
+);
+
+export default Features;
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Feature from '../../components/feature/Feature';
+import './whatGPT3.css';
+
+const WhatGPT3 = () => (
+  <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+    <div className="gpt3__whatgpt3-feature">
+      <Feature title="What is GPT-3" text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo." />
+    </div>
+    <div className="gpt3__whatgpt3-heading">
+      <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+      <p>Explore the Library</p>
+    </div>
+    <div className="gpt3__whatgpt3-container">
+      <Feature title="Chatbots" text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo." />
+      <Feature title="Knowledgebase" text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo." />
+      <Feature title="Education" text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo." />
+    </div>
+  </div>
+);
+
+export default WhatGPT3;
